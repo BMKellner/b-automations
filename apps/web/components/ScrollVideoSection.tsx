719c2a6d268c5b98a267e7859b95315c459a5b9c
BMKellner <145494653+BMKellner@users.noreply.github.com>
2025-11-19 16:22:22 -0500

    Update ScrollVideoSection.tsx

--- apps/web/components/ScrollVideoSection.tsx
+++ apps/web/components/ScrollVideoSection.tsx
@@ -123,16 +123,6 @@ export default function ScrollVideoSection({
               );
             })}
           </div>
-
-          {/* Scroll indicator */}
-          {scrollProgress < 0.95 && isReady && (
-            <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-20 animate-bounce">
-              <div className="text-white/60 text-sm">Scroll to explore</div>
-              <div className="w-6 h-10 border-2 border-white/60 rounded-full mx-auto mt-2 flex justify-center">
-                <div className="w-1 h-2 bg-white/60 rounded-full mt-2 animate-pulse" />
-              </div>
-            </div>
-          )}
         </div>
       </div>
 
